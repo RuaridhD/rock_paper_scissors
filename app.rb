142bc5ca1027d513d86dc690a9021a63cc690417
Ruaridh <ruaridhdunbar@gmail.com>
2018-04-16 22:07:50 +0100

    add welcome page, html layout

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require("sinatra")
 require("sinatra/contrib/all") if development?
-
+also_reload("./models/*")
 require_relative("models/game.rb")
+
+get "/" do
+  erb(:welcome)
+end
 
 get "/:hand1/:hand2" do
   @winner = Game.winner(params[:hand1].to_s, params[:hand2].to_s)
+  erb(:winner)
 end
--- models/game.rb
+++ models/game.rb
@@ -3,17 +3,17 @@ class Game
   def self.winner(hand1, hand2)
 
     if hand1.downcase == "rock" && hand2.downcase == "scissors"
-      return "Rock wins"
+      return "Rock"
     elsif hand1.downcase == "rock" && hand2.downcase == "paper"
-      return "Paper wins"
+      return "Paper"
     elsif hand1.downcase == "scissors" && hand2.downcase == "paper"
-      return "Scissors wins"
+      return "Scissors"
     elsif hand1.downcase == "scissors" && hand2.downcase == "rock"
-      return "Rock wins"
+      return "Rock"
     elsif hand1.downcase == "paper" && hand2.downcase == "rock"
-      return "Paper wins"
+      return "Paper"
     elsif hand1.downcase == "paper" && hand2.downcase == "scissors"
-      return "Scissors wins"
+      return "Scissors"
     else
       return "You can only play as rock, paper or scissors."
     end
